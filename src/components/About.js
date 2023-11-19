895d8f966204d7b5920d1c0c3377ef733515bf2d
nabdi7 <36966910+nabdi7@users.noreply.github.com>
2023-11-18 17:22:05 -0800

    first commit

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,29 @@ const About = () => {
         <>
         <section className='about'>
             <h1>About</h1>
-            <p> Hadiya Home Childcare welcomes children of all abilities, with the commitment to provide quality care and education to each child. We believe every child is a unique individual with varying needs, and we welcome the diversity that brings to our center. Our goal is to make Hadiya Home Childcare a safe and welcoming place for children to have fun and be themselves. We want children to be able express themselves and feel accepted. All children and families deserve to be full members of their communities, to have the opportunity for development and learning, and to experience a sense of belonging.</p>
+            <p> Hadiya Home Childcare welcomes children of all abilities, 
+                with the commitment to provide quality care and education 
+                to each child. We believe every child is a unique individual 
+                with varying needs, and we welcome the diversity that brings 
+                to our center. Our goal is to make Hadiya Home Childcare a safe 
+                and welcoming place for children to have fun and be themselves. 
+                We want children to be able express themselves and feel accepted. 
+                All children and families deserve to be full members of their communities, 
+                to have the opportunity for development and learning, and to experience 
+                a sense of belonging.</p>
+            <p> At Hadiya Home Childcare, we envision a world where every child has 
+                access to quality early childhood education in a safe and inclusive 
+                environment. We believe that every child has the potential to achieve 
+                great things, and we’re committed to helping them discover and develop 
+                their unique strengths. Our goal is to create a community of lifelong 
+                learners who are confident, curious and compassionate. We’re dedicated 
+                to fostering a love of learning that will carry our children through 
+                their lives. </p>
+                
+            <p> Let’s talk. </p>
+                
+            <p> contact@domain.com </p>
+            
         </section>
 
         </>
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,33 +1,72 @@
 import React from "react"; 
 import "./Services.css";
+import servicePic from '../images/Preschool.jpeg';
 
 const Services = () => {
     return (
         <>
+        {/* add a fingers with color image on both sides */}
+        <div className='left-section'></div>
+        <div className='right-section'> </div>
+
         <section className='services'>
-            <h1>Services</h1>
+            <h1 className='page-title'>Programs</h1>
+            <div className='service-image'>
+                <img src={servicePic} alt=''/>
+            </div>
             <div className='services-container'>
-                <div className='services-content'>
-                    <h2 className='services-title'>Our Services</h2>
-                    <p className='services-description'>Lorem ipsum</p>
+                <div className='our-services'>
+                    <div className='services-content'>
+                        {/* <h2 className='services-title'>Our Services</h2> */}
+                        <p className='services-description'>Welcome to Hadiya Home Childcare, 
+                        where we prioritize the well-being and development of your little ones. 
+                        Our range of services is designed to create a nurturing and engaging 
+                        environment, ensuring a positive and enriching experience for your child.</p>
+                    </div>
                 </div>
-                <div className='services-content'>
-                    <h2 className='services-title'>The basics</h2>
-                    <p className='services-description'>Lorem ipsum</p>
+            
+                <div className='halves-container'>
+                    <div className='services-half'>
+                        <h2 className='services-title'>The basics</h2>
+                        <p className='services-description'>At Hadiya Home Childcare, 
+                        we believe in the fundamentals. From a structured daily routine to 
+                        nutritious meals, we cover the essentials to ensure a well-rounded 
+                        and healthy experience for your child.</p>
+                        {/* <div className='left-section'></div> */}
+                        <ul className='basics'>
+                            <li>Part-time, Full-time</li>
+                            <li>Snacks Provided</li>
+                            <li>Meals Provided</li>
+                            <li>Certified in First Aid, CPR, teaching, and Early Childhood Education</li>
+                        </ul>
+                    </div>
+                    <div className='services-half'>
+                        <h2 className='services-title'>Safety</h2>
+                        <p className='services-description'>Your child's safety is our top priority. 
+                        Rest easy knowing that our staff is CPR and First Aid certified, 
+                        providing individual attention and constant monitoring to create a 
+                        secure and protected environment.</p>
+                        <div className='right-section'> </div>
+                    </div>
                 </div>
-                <div className='services-content'>
-                    <h2 className='services-title'>Safety</h2>
-                    <p className='services-description'>CPR & First Aid certified
-                    Individual attention
-                    Staff monitoring safety</p>
-                </div>
-                <div className='services-content'>
-                    <h2 className='services-title'>Tuition</h2>
-                    <p className='services-description'>Lorem ipsum</p>
-                </div>
-                <div className='services-content'>
-                    <h2 className='services-title'>Teachers</h2>
-                    <p className='services-description'>Lorem ipsum</p>
+                <div className='halves-container'>
+                    <div className='services-half'>
+                        <h2 className='services-title'>Tuition</h2>
+                        <p className='services-description'>Please contact us. DCYF and Private pay accepted.</p>
+                        
+                    </div>
+                    <div className='services-half'>
+                        <h2 className='services-title'>Teachers</h2>
+                        <p className='services-description'>As a dedicated childcare provider 
+                        with extensive experience from raising my own children to mentoring in a 
+                        childcare facility to attain the necessary skills, I'm devoted to 
+                        providing children with the tools they need to thrive and succeed.
+                        With my professional experience and certifications in First Aid, CPR, 
+                        teaching, and Early Childhood Education, I'm highly accomplished in 
+                        teaching both educational and intentional play activities designed to 
+                        develop children's' psychological, emotional, behavioral, and social 
+                        skills..</p>
+                    </div>
                 </div>
             </div>
         </section>
